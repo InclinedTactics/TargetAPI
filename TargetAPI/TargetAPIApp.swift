82d6c9cefec4632a96d1e4f969d2e39ebbe75567
Joseph DeWeese <inclinedtactics@icloud.com>
2024-04-18 15:02:25 -0500

    Initial Commit

---
+++ TargetAPI/TargetAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TargetAPIApp.swift
+//  TargetAPI
+//
+//  Created by Joseph DeWeese on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct TargetAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
